63caf1372e27ddcf8e1ee512f2713af3223b876b
Anusha Srivastva <Anushasrivastava07@gmail.com>
2023-08-22 22:02:49 +0530

    introduced player x and player 0 logic on board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,20 +3,27 @@ import { useState } from "react"
 const Board =()=>{
 
 const [square,setSquare]=useState(Array(9).fill(null))   ;
+//create state for player
+const [isXNext,setIsXNext] = useState(false);
 const handleSquareClick = clickedPosition => {
-
+      //to prevent chnaging of value once the square is filled
+      if(square[clickedPosition]){
+          return;
+      }
       //set value in sq as per logic
       setSquare( currentSquare => {
         return currentSquare.map((squareValue,position)=>{
-          if(clickedPosition==position){
-               return "x";
+          if(clickedPosition === position){
+               return isXNext ? 'X' : '0';
           }
 
           return squareValue;
 
         });
       });
+      setIsXNext((currentIsXNext)=> !currentIsXNext);
 };
+
 
 
 const renderSquare = position => {
